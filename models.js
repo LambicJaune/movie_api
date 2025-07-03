100143c0247875355cb08c6be80cbc385013542c
LambicJaune <thehopman@outlook.com>
2025-07-03 10:34:53 +0200

    Created model.js

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let movieSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {
+        name: String,
+        description: String
+    },
+    director: {
+        name: String,
+        bio: String,
+        birth: String,
+        death: String
+    },
+    imagePath: String,
+    featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
