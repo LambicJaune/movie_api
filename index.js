16cc89e786e19be6dd737d9616de2d129277fc8c
LambicJaune <thehopman@outlook.com>
2025-07-07 13:35:19 +0200

    Added Cors

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ const express = require('express'),
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 //import auth.js file (creates new endpoint for registered users to login)
 let auth = require('./auth')(app);
 
