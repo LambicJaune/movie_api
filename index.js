469049aae677421be70d8742229f68989525de55
LambicJaune <thehopman@outlook.com>
2025-09-23 10:36:09 +0200

    Enabled CORS for Angular frontend

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://mytomhanksapp.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://mytomhanksapp.netlify.app', 'http://localhost:4200'];
 
 app.use(cors({
     origin: (origin, callback) => {
