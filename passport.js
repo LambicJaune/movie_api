5b11aedc5660dc440f770572a626f45af559b8b5
LambicJaune <thehopman@outlook.com>
2025-09-24 16:39:00 +0200

    Updated Local strategy to fit Angular form / edit user

--- passport.js
+++ passport.js
@@ -8,37 +8,38 @@ let Users = Models.User,
     ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
-    new LocalStrategy(
-        {
-            usernameField: 'Username',
-            passwordField: 'Password',
-        },
-        async (username, password, callback) => {
-            console.log(`${username} ${password}`);
-            await Users.findOne({ Username: username })
-                .then((user) => {
-                    if (!user) {
-                        console.log('incorrect username');
-                        return callback(null, false, {
-                            message: 'Incorrect username or password.',
-                        });
-                    }
-                    if (!user.validatePassword(password)) {
-                        console.log('incorrect password');
-                        return callback(null, false, { message: 'Incorrect password.' });
-                    }
-                    console.log('finished');
-                    return callback(null, user);
-                })
-                .catch((error) => {
-                    if (error) {
-                        console.log(error);
-                        return callback(error);
-                    }
-                })
+  new LocalStrategy(
+    {
+      usernameField: 'username', // matches frontend
+      passwordField: 'password',
+    },
+    async (username, password, callback) => {
+      console.log(`${username} ${password}`);
+      try {
+        const user = await Users.findOne({ Username: username }); // still capitalized in DB
+
+        if (!user) {
+          console.log('incorrect username');
+          return callback(null, false, {
+            message: 'Incorrect username or password.',
+          });
         }
-    )
+
+        if (!user.validatePassword(password)) {
+          console.log('incorrect password');
+          return callback(null, false, { message: 'Incorrect password.' });
+        }
+
+        console.log('finished');
+        return callback(null, user);
+      } catch (error) {
+        console.error(error);
+        return callback(error);
+      }
+    }
+  )
 );
+
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
